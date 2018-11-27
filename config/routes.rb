44312438716d34c825b6b97d45aacbdb348d375f
Clement Bouakkaz <clementbouakkaz@gmail.com>
2018-11-27 11:59:48 +0000

    implemented routes index et show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # 1. list all the tasks
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
