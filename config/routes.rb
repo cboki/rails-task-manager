fc449925e67eccad253274314afe26036ff19974
Clement Bouakkaz <clementbouakkaz@gmail.com>
2018-11-27 14:49:15 +0000

    adding new and edit for creating and update task

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   # 1. list all the tasks
   get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new'
   get 'tasks/:id', to: 'tasks#show', as: 'task'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'task_edit'
+  patch 'tasks/:id', to: 'tasks#update'
 end
